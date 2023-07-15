c81f3b8450f0d692d8d3d2c149df361e0fac19c0
amitpatilk <79409807+amitpatilk@users.noreply.github.com>
2023-07-15 00:49:54 -0700

    Final-Year-BlockChain-Project

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <h1>SUPPLYCHAIN LOGISTICS MANAGEMENT BASED ON ETHEREUM</h1>
+      <br />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/owner">Owner</Link>
+        </li>
+        <li>
+          <Link to="/manufacturer">Manufacturer</Link>
+        </li>
+        <li>
+          <Link to="/distributor">Distributor</Link>
+        </li>
+        <li>
+          <Link to="/retailer">Retailer</Link>
+        </li>
+        <li>
+          <Link to="/consumer">Consumer</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Home;
